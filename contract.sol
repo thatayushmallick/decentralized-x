4eb547871a9bbef17878ffb00c7680ee282e9672
Ameya16260 <134862779+Ameya16260@users.noreply.github.com>
2025-04-11 00:07:29 +0530

    updated contract.sol

--- contract.sol
+++ contract.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.12;
 
 contract appcontract{
     uint public postId=0;
+
+
+
+
     struct User{
         address userAddress;
         string username;
@@ -15,9 +19,24 @@ contract appcontract{
         string hash;
         uint id;
     }
+
+
+
+
     mapping (uint=>post) public posts;
     mapping(address=>bool) isRegistered;
     mapping(address=>User) public users;
+
+    modifier postExists(uint _postId){
+        require(_postId>0 && _postId <= postId, "Post does not exist");
+        _;
+    }
+
+
+    event userRedistered(address indexed user, string username);
+    event postCreated(uint indexed postId, address indexed author, string hash);
+
+
     function register(string memory _username) public {
         require(!isRegistered[msg.sender], "User is already registered");
         User storage newUser = users[msg.sender]; 
@@ -25,9 +44,12 @@ contract appcontract{
         newUser.username = _username;
         newUser.postCount=0;
         isRegistered[msg.sender]=true;
+
+        emit userRedistered(msg.sender, _username);
     }
 
     function createPost(string memory _hash) public {
+        require(isRegistered[msg.sender], "User need to register first");
         postId++;
         posts[postId]=post({
 
@@ -38,8 +60,11 @@ contract appcontract{
         });
         users[msg.sender].userPosts.push(postId);
         users[msg.sender].postCount++;
+
+        emit postCreated(postId, msg.sender, _hash);
     }
-    function getUserPosts(address _user) public view returns (uint[] memory){
-        return users[_user].userPosts;
+    function getPost(uint _postId) public view postExists(_postId) returns (address, uint, string memory, uint) {
+    post memory p = posts[_postId];
+    return (p.author, p.postedAt, p.hash, p.id);
     }
 }
